e1297e3f3b8b3f558de1b77c4101d17e74cf889d
Johni12M <jonathan.metz999@icloud.com>
2025-09-22 20:31:19 +0200

    added text

--- public/js/core.bundle.de.js
+++ public/js/core.bundle.de.js
@@ -6612,6 +6612,20 @@ function initIndex() {
         
         content: "Dies ist ein Testartikel."
       })
+      .add(
+      
+      
+      
+      
+      {
+        id: 2,
+        href: "/snow_project/snow/wie_entsteht_schnee/",
+        title: "Wie ensteht Schnee?",
+        description: "Erfahre, wie Schnee entsteht, von der Verdunstung bis zur Kristallisation. Entdecke die faszinierenden Prozesse hinter Schneeflocken und ihre verschiedenen Formen.",
+        
+        
+        content: "Wie entsteht Schnee? Schnee fällt auf den Boden. Es ist wieder Dezember und die Schneeflocken bedecken wie jedes Jahr den Boden. Doch viele Menschen fragen sich immer wieder, wie der Schnee eigentlich entsteht. Im Grunde ist es gar nicht so schwer.\nVerdunstung Das Wasser, das sich am Boden auf der Erde befindet, verdunstet von der Erdoberfläche und steigt in die Atmosphäre auf. Dort, in der Höhe, kondensiert der Wasserdampf und bildet Wolken. Die sogenannte Kondensation ist der Übergang eines Stoffes vom gasförmigen in den flüssigen Aggregatzustand.\nKristallisation Der weitere Prozess startet erst bei bestimmten Temperaturen, die ungefähr -5 Grad betragen, beginnen. In den gebildeten Wolken befinden sich feine Tröpfchen von unterkühltem Wasser. Die Tröpfchen lagern sich an Kristallisationskeime, die zum Beispiel Staubteilchen, Pollen oder Salz sein können, und gefrieren. Die daraufhin entstandenen Eiskristalle haben einen kleineren Durchmesser als 0,1mm. Diese fallen durch eine immer weiter zunehmende Masse nach unten. Die Eiskristalle können weiter anwachsen, was durch den unterschiedlichen Dampfdruck von Eis und unterkühltem Wasser passiert.\nBergeron-Findeisen-Prozess Wenn der in der Luft enthaltene Wasserdampf resublimiert und in Eis übergeht, trägt dies zum Kristallwachstum bei. Dies nennt man den „Bergeron-Findeisen-Prozess.“\nSchneeflocken Die endgültigen Schneeflocken werden dann in Wolken gebildet, sobald die Temperatur unter den Gefrierpunkt fällt und unterkühlte Wassertröpfchen und Wasserdampf, die an den Kristallen anlagern, vorhanden sind. Wenn es eher wärmer ist, werden die einzelnen Wasser- oder Schneetropfen miteinander verklebt und es entstehen Schneeflocken, die an einen Wattebausch erinnern. Bei tiefen Temperaturen bilden sich nur sehr kleine Flocken, was Schneegriesel genannt wird. Je höher die Temperatur ist, desto größer werden auch die Schneeflocken, denn die Kristalle tauen an und verkleben sich zu großen Flocken.\nStammformen der Schneekristalle Es gibt verschiedene Stammformen der Schneekristalle. Diese hängen von der Temperatur ab. Bei tiefen Temperaturen entstehen, wie zum Beispiel -10 bis -20 °C, Plättchen oder Prismen. Bei eher höheren Temperaturen, wie -2 °C, bilden sich sechsarmige Dendriten, die man Eissterne oder Schneesterne nennt.\nNatürlich gibt es auch noch andere Formen wie Nadeln und Säulen, welche bei noch höheren und tieferen Temperaturen erscheinen.\nVoraussetzungen Es gibt einige Voraussetzungen, damit Schnee überhaupt entstehen kann. Wichtig ist, dass eine ausreichende Luftfeuchtigkeit und eine Temperatur unter 0 °C vorhanden ist. Außerdem sollten die Wolken über genügend Luftkeime verfügen, damit Schneeflocken jeden Winter wieder auf den Boden fallen können."
+      })
       ;
   
 
